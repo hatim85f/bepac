9af1d9c26864f0721ba358f6b51638a57720843f
hatim85f <hatim.fayez85@gmail.com>
2023-01-04 22:51:19 +0400

    'pushing'

--- server.js
+++ server.js
@@ -1,12 +1,11 @@
 const express = require("express");
 const cors = require("cors");
 const connectDB = require("./config/db");
+const path = require("path");
 
 const app = express();
 
 connectDB();
-
-app.get("/", (req, res) => res.send("API Running"));
 
 const PORT = process.env.PORT || 5000;
 
@@ -18,5 +17,15 @@ app.use("/api/registeration", require("./routes/api/users"));
 app.use("/api/admin_create", require("./routes/api/adminCreate"));
 app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/admin", require("./routes/api/admin"));
+
+// serve static assets in production
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("bepacedu/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "bepacedu", "build", "index.html"));
+  });
+}
 
 app.listen(PORT, () => console.log(`Server started on Port ${PORT}`));
